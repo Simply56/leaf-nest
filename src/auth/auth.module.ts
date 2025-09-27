b2d519eafe6c5f6a889de163828423fbf2e993ad
Martin Srsen <xsrsen1@fi.muni.cz>
2025-09-27 20:21:57 +0200

    add jwt auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { AuthGuard } from './auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+  exports: [AuthService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    })
+  ]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private jwtService: JwtService
+    ) { }
+    async signIn(pass: string): Promise<{ access_token: string; }> {
+        if (pass !== '1234') {
+            throw new UnauthorizedException();
+        }
+
+        return {
+            access_token: await this.jwtService.signAsync({ sub: "all" }),
+        };
+
+    }
+}
