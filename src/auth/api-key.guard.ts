5157fdcdbc86ffafc8549c326fcc02e70a2c0c28
Martin Srsen <xsrsen1@fi.muni.cz>
2025-09-28 13:22:15 +0200

    use env variable for auth

--- src/auth/api-key.guard.ts
+++ src/auth/api-key.guard.ts
@@ -1,15 +1,22 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  private readonly validApiKey = '123';
+  private readonly validApiKey = process.env['leafApiKey'];
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const apiKey = request.query.apiKey;
 
     if (!apiKey) {
-      throw new UnauthorizedException('API key is required. Add ?apiKey=your_key to the URL');
+      throw new UnauthorizedException(
+        'API key is required. Add ?apiKey=your_key to the URL',
+      );
     }
 
     if (apiKey !== this.validApiKey) {
